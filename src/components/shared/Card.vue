44745389866a9e162b76b7af7cd41bc4e1e46531
mehdi-larek <larek.med@gmail.com>
2026-01-30 13:31:30 +0100

    feat: ajout de composant InputText et Card

---
+++ src/components/shared/Card.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+export default {
+  props: {
+    width: {
+      type: String,
+      required: false,
+      default: 'min-w-50',
+    },
+    height: {
+      type: String,
+      required: false,
+      default: 'min-h-48',
+    },
+  },
+};
+</script>
+
+<template>
+  <article :class="`${width} ${height} bg-white border rounded-lg shadow-md p-6 flex flex-col`">
+    <slot></slot>
+  </article>
+</template>
