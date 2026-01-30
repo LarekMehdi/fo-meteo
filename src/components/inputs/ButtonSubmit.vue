90a4cfc0618c97f1f02e6ef2a69f568e9caa29fc
mehdi-larek <larek.med@gmail.com>
2026-01-30 13:52:06 +0100

    feat: ajout de composant ButtonSubmit

---
+++ src/components/inputs/ButtonSubmit.vue
@@ -0,0 +1,44 @@
+<script lang="ts">
+export default {
+  props: {
+    buttonColor: {
+      type: String,
+      required: false,
+      default: 'bg-blue-600',
+    },
+    buttonColorHover: {
+      type: String,
+      required: false,
+      default: 'hover:bg-blue-700',
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  emits: ['click'],
+  setup(props, { emit }) {
+    const handleClick = () => {
+      emit('click');
+    };
+    return {
+      props,
+      handleClick,
+    };
+  },
+};
+</script>
+
+<template>
+  <button
+    type="submit"
+    :class="[
+      props.buttonColor,
+      props.buttonColorHover,
+      'py-2 px-4 rounded active:shadow-lg min-w-[150px] text-white transition-colors',
+    ]"
+    @click="handleClick"
+  >
+    {{ props.content }}
+  </button>
+</template>
