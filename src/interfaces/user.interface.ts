d6f7bc12c84b89ddef27274b5e58c889f56be7a5
mehdi-larek <larek.med@gmail.com>
2026-01-30 14:18:09 +0100

    feat: ajout du authStore

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -10,3 +10,16 @@ export interface SigninInterface {
   email: string | null;
   password: string | null;
 }
+
+export interface AuthUserInterface {
+  id: number;
+  firstname: string;
+  lastname: string;
+  email: string;
+}
+
+export interface AuthStoreInterface {
+  user: AuthUserInterface | null;
+  accessToken: string | null;
+  refreshToken: string | null;
+}
