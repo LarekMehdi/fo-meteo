14cdf91ac563534b8ffe5110592718483203eafe
mehdi-larek <larek.med@gmail.com>
2026-02-01 13:47:42 +0100

    feat: composant FormRow

---
+++ src/components/shared/FormRow.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+export default {
+  props: {
+    cols: {
+      type: Number,
+      required: false,
+      default: 1,
+    },
+  },
+};
+</script>
+
+<template>
+  <section class="grid gap-4" :class="cols === 2 ? 'grid-cols-1 lg:grid-cols-2' : 'grid-cols-1'">
+    <slot />
+  </section>
+</template>
