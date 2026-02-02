e129d85956ac491e46e59529005478d261fa8faf
mehdi-larek <larek.med@gmail.com>
2026-02-02 13:54:56 +0100

    core: ajout de tanstack query + debounce

--- src/main.ts
+++ src/main.ts
@@ -13,15 +13,21 @@ import piniaPersist from 'pinia-plugin-persistedstate';
 import Toast, { POSITION, type PluginOptions } from 'vue-toastification';
 import 'vue-toastification/dist/index.css';
 
+/** TANSTACK QUERY **/
+import { VueQueryPlugin, QueryClient } from '@tanstack/vue-query';
+
 const options: PluginOptions = {
   position: POSITION.TOP_CENTER,
 };
 const pinia = createPinia();
 pinia.use(piniaPersist);
 
+const queryClient = new QueryClient();
+
 const app = createApp(App);
 app.use(pinia);
 app.use(Toast, options);
 app.use(router);
+app.use(VueQueryPlugin, { queryClient });
 
 app.mount('#app');
