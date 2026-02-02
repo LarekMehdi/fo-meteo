a6ec4f8985fd44251138b3890958f2350b8b2cf3
mehdi-larek <larek.med@gmail.com>
2026-02-02 14:17:42 +0100

    feat: appel api pour la ville

--- src/api/forecast.api.ts
+++ src/api/forecast.api.ts
@@ -1,6 +1,7 @@
 import type { ForecastFilterInterface } from '@/interfaces/filter.interface';
 import { useApi } from './useApi';
 import type { ForecastInterface } from '@/interfaces/forecast.interface';
+import type { City } from '@/interfaces/city.interface';
 
 export abstract class ForecastApi {
   /** FORECAST **/
@@ -8,6 +9,16 @@ export abstract class ForecastApi {
     try {
       const { data } = await useApi().get(`/forecast`, { params: filter });
       console.log(data);
+      return data;
+    } catch (e: unknown) {
+      throw e;
+    }
+  }
+
+  /** CITY **/
+  static async searchCity(name: string): Promise<City[]> {
+    try {
+      const { data } = await useApi().get(`/forecast/city`, { params: { name } });
       return data;
     } catch (e: unknown) {
       throw e;
--- src/services/forecast.service.ts
+++ src/services/forecast.service.ts
@@ -1,4 +1,5 @@
 import { ForecastApi } from '@/api/forecast.api';
+import type { City } from '@/interfaces/city.interface';
 import type { ForecastFilterInterface } from '@/interfaces/filter.interface';
 import type { ForecastInterface } from '@/interfaces/forecast.interface';
 
@@ -6,5 +7,10 @@ export abstract class ForecastService {
   /** FORECAST **/
   static async getForecast(filter: ForecastFilterInterface): Promise<ForecastInterface> {
     return await ForecastApi.getForecast(filter);
+  }
+
+  /** CITY **/
+  static async searchCity(name: string): Promise<City[]> {
+    return await ForecastApi.searchCity(name);
   }
 }
