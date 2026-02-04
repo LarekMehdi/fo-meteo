b78edf4e2c57ecf9afd1a0a9b17a754ae74a7edc
mehdi-larek <larek.med@gmail.com>
2026-02-04 11:17:17 +0100

    feat: protection des routes en étant connecté

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import SigninView from '@/views/auth/SigninView.vue';
 import SignupView from '@/views/auth/SignupView.vue';
 import ForecastView from '@/views/ForecastView.vue';
 import HistoryView from '@/views/HistoryView.vue';
+import { useAuthStore } from '@/stores/auth.store';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,12 +19,14 @@ const router = createRouter({
       path: '/forecast',
       name: 'forecast',
       component: ForecastView,
+      meta: { requiresAuth: true },
     },
     // HISTORIQUE
     {
       path: '/history',
       name: 'history',
       component: HistoryView,
+      meta: { requiresAuth: true },
     },
     // AUTH
     {
@@ -37,6 +40,20 @@ const router = createRouter({
       component: SignupView,
     },
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  const { isLoggedIn } = useAuthStore();
+
+  if (to.meta.requiresAuth && !isLoggedIn) {
+    return next({ name: 'signin' });
+  }
+
+  if ((to.name === 'signin' || to.name === 'signup') && isLoggedIn) {
+    return next({ name: 'home' });
+  }
+
+  return next();
 });
 
 export default router;
